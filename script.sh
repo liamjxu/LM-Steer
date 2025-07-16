79b47f2c3c1e1f5451e1253d4f57da3aea88f966
Jialiang Xu <xjl@stanford.edu>
2025-07-16 15:23:21 -0700

    script

--- script.sh
+++ script.sh
@@ -18,9 +18,9 @@ PYTHONPATH=. python experiments/training/generate.py \
     --adaptor_class multiply --num_steers 2 --rank 1000 \
     --max_length 256 --verbose --steer_values ${control} 1 --top_p 0.9
 
-# python experiments/evaluation/evaluate.py \
-#     --generations_file logs/$TRIAL/predictions-${source}_${control}.jsonl \
-#     --metrics sentiment,ppl-big,dist-n \
-#     --output_file result_stats_${source}_${control}.txt
-# echo "Sentiment control results:"
-# cat logs/$TRIAL/result_stats_${source}_${control}.txt
+python experiments/evaluation/evaluate.py \
+    --generations_file logs/$TRIAL/predictions-${source}_${control}.jsonl \
+    --metrics sentiment,ppl-big,dist-n \
+    --output_file result_stats_${source}_${control}.txt
+echo "Sentiment control results:"
+cat logs/$TRIAL/result_stats_${source}_${control}.txt
