dce9630e26c8460d20f28fb5efd12a4fe4464709
Jialiang Xu <xjl@stanford.edu>
2025-07-16 15:10:04 -0700

    gen neutral -5

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 TRIAL=sentiment-gpt2-large
 mkdir -p logs/$TRIAL
 
-source=positive
+source=neutral
 control=-5
 # PYTHONPATH=. python experiments/training/train.py \
 #     --dataset_name sentiment-sst5 \
@@ -18,9 +18,9 @@ PYTHONPATH=. python experiments/training/generate.py \
     --adaptor_class multiply --num_steers 2 --rank 1000 \
     --max_length 256 --verbose --steer_values ${control} 1 --top_p 0.9
 
-python experiments/evaluation/evaluate.py \
-    --generations_file logs/$TRIAL/predictions-${source}_${control}.jsonl \
-    --metrics sentiment,ppl-big,dist-n \
-    --output_file result_stats_${source}_${control}.txt
-echo "Sentiment control results:"
-cat logs/$TRIAL/result_stats_${source}_${control}.txt
+# python experiments/evaluation/evaluate.py \
+#     --generations_file logs/$TRIAL/predictions-${source}_${control}.jsonl \
+#     --metrics sentiment,ppl-big,dist-n \
+#     --output_file result_stats_${source}_${control}.txt
+# echo "Sentiment control results:"
+# cat logs/$TRIAL/result_stats_${source}_${control}.txt
